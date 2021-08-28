94a6f9194b8b8cacbae57f425232d47379a3d0c9
Parikshit Patil <patil8698parikshit@gmail.com>
2021-08-28 21:40:46 +0530

    react props

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail/>
+      <CommentDetail author={faker.name.firstName()} time={faker.date.past().toString()} comment={faker.lorem.sentence()}/>
+      <CommentDetail author={faker.name.firstName()} time={faker.date.past().toString()} comment={faker.lorem.sentence()}/>
+      <CommentDetail author={faker.name.firstName()} time={faker.date.past().toString()} comment={faker.lorem.sentence()}/>
+      <CommentDetail author={faker.name.firstName()} time={faker.date.past().toString()} comment={faker.lorem.sentence()}/>
     </div>
   );
 }
