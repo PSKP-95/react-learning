4e6e13d3362d3d13d0a39e83d6ae6e2a7bf8fd57
Parikshit Patil <patil8698parikshit@gmail.com>
2021-08-29 20:49:53 +0530

    :recycle: cleaned project for image seach project

--- src/index.js
+++ src/index.js
@@ -1,52 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import App from './components/App';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' }; // babel create constructor for us
-  // not required
-  // get called first
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { lat: null, errorMessage: '' };
-  // }
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState( {lat: position.coords.latitude} );
-        console.log(position);
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
-
-  componentDidUpdate() {
-    console.log("Component updated")
-  }
-
-  renderContent(){
-    if(this.state.errorMessage && !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>
-    } else if(!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
-    } else {
-      return <div><Spinner message="pls accept location request"/></div>
-    }
-  }
-
-  // required function
-  render() {
-    return (
-      <div>
-        {this.renderContent()}
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <React.StrictMode>
