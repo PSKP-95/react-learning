c9f67469c179daab90915264da470892eff8809e
Parikshit Patil <patil8698parikshit@gmail.com>
2021-08-29 14:29:28 +0530

    season app completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }; // babel create constructor for us
@@ -26,15 +28,23 @@ class App extends React.Component {
     console.log("Component updated")
   }
 
-  // required function
-  render() {
+  renderContent(){
     if(this.state.errorMessage && !this.state.lat){
       return <div>Error: {this.state.errorMessage}</div>
     } else if(!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     } else {
-      return <div>Loading</div>
+      return <div><Spinner message="pls accept location request"/></div>
     }
+  }
+
+  // required function
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
